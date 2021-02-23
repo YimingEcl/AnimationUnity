cc064ad68cc26870b07cf39b478987097b86dd90
Fu Yiming <fu.yiming@entcomp.esys.tsukuba.ac.jp>
2021-02-23 19:50:42 +0900

    Add Eigen Library.

---
+++ Assets/Plugins/Eigen/EigenSample.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EigenSample : MonoBehaviour
+{
+    void Start()
+    {
+        System.IntPtr ptr = Eigen.Create(2, 3);
+        Debug.Log(Eigen.GetRows(ptr));
+    }
+}
